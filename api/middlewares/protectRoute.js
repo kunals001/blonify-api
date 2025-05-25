0752efc78463e3a21475b2bacd922fbc810c10a0
kunals001 <kunaltrad555@gmail.com>
2025-05-25 14:30:30 +0530

    remove

--- api/middlewares/protectRoute.js
+++ api/middlewares/protectRoute.js
@@ -7,7 +7,7 @@ export const protectRoute = async (req, res, next) => {
         const token = req.cookies.token;
 
         if(!token){
-            return res.status(401).json({success:false,message:"Not authorized"})
+            return res.status(401).json({success:false,message:""})
         }
 
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
