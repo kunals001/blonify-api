89fb6f3b2ba8e2df49bcaf29e007cb4f5fcc7033
kunals001 <kunaltrad555@gmail.com>
2025-06-20 09:50:34 +0530

    Finally

--- api/middlewares/protectRoute.js
+++ api/middlewares/protectRoute.js
@@ -12,11 +12,6 @@ export const protectRoute = async (req, res, next) => {
 
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
         const user = await User.findById(decoded.userId);
-
-        if(!user){
-            return res.status(401).json({success:false,message:"Not authorized"})
-        }
-
         req.user = user;
         next();
     } catch (error) {
